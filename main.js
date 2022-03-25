2f5609713596068e4ab659808a878b5eae5a0fbf
theh2so4 <Yehiker0925@gmail.com>
2022-03-25 22:43:01 +0000

    Lorax [1.0.0]
    [+] First commit

---
+++ main.js
@@ -0,0 +1,67 @@
+// # /////////////////////////////////////////////////////////////////////////// # //
+// #                                                                             # //
+// #                      Copyright 2022 TheH2SO4                                # //
+// #                                                                             # //
+// #   Licensed under the Apache License, Version 2.0 (the 'License');           # //
+// #   you may not use this file except in compliance with the License.          # //
+// #   You may obtain a copy of the License at                                   # //
+// #                                                                             # //
+// #       http://www.apache.org/licenses/LICENSE-2.0                            # //
+// #                                                                             # //
+// #   Unless required by applicable law or agreed to in writing, software       # //
+// #   distributed under the License is distributed on an 'AS IS' BASIS,         # //
+// #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  # //
+// #   See the License for the specific language governing permissions and       # //
+// #   limitations under the License.                                            # //
+// #                                                                             # //
+// # /////////////////////////////////////////////////////////////////////////// # //
+
+"use strict"
+
+// # ////////////////| [🥽] | TheH2SO4 | [⚗️] |//////////////// # //
+
+                  //////////////////////////////////
+                  //  _                           //
+                  // | |                          //
+                  // | |     ___  _ __ __ ___  __ //
+                  // | |    / _ \| '__/ _` \ \/ / //
+                  // | |___| (_) | | | (_| |>  <  //
+                  // |______\___/|_|  \__,_/_/\_\ //
+                  //                              //
+                  //////////////////////////////////
+
+// # ////////////////| [📚] - Libraries
+
+const fs = require('fs')
+const { exec } = require('child_process')
+const yaml = require('js-yaml')
+
+// # ////////////////| [📓] - Files
+
+const settings = yaml.load(fs.readFileSync('./settings.yml', 'utf8'))
+
+// # ////////////////| [🧪] - Constants
+
+
+
+// # ////////////////| [🍣] - Variables
+
+
+
+// # ////////////////| [☢️] - Start
+
+
+
+// # ////////////////| [🌧️] - End
+
+                  //////////////////////////////////
+                  //  _                           //
+                  // | |                          //
+                  // | |     ___  _ __ __ ___  __ //
+                  // | |    / _ \| '__/ _` \ \/ / //
+                  // | |___| (_) | | | (_| |>  <  //
+                  // |______\___/|_|  \__,_/_/\_\ //
+                  //                              //
+                  //////////////////////////////////
+
+// # Thanks for using Lorax! <3
